280dbd256b3a28f1f6fe34ec14d938ed7f04b908
Noah Naizir <Itahand.naizir@gmail.com>
2025-12-02 17:22:37 -0700

    Test suite init

---
+++ tasks/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	//if you imports this with .  you do not have to repeat overflow everywhere
+	. "github.com/bjartek/overflow/v2"
+	"github.com/fatih/color"
+)
+
+// ReadFile reads a text file and returns an array of paragraphs
+
+func main() {
+	o := Overflow(
+		WithGlobalPrintOptions(),
+		// WithNetwork("testnet"),
+	)
+
+	fmt.Println("Testing Contract")
+
+	color.Blue("Mneme Contract testing")
+
+	color.Green("Setup bob account for ArtDrop collection")
+	// Setup artist account for ArtDrop collection
+	o.Tx("Mneme/setup",
+		WithSigner("bob"),
+	).Print()
+
+}
